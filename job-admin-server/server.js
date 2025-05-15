c1a9a8a1ef5a39dc4e5ef8cb4e065f993c31cef1
KrishnaPrashanth08 <prashanthsura08.com>
2025-05-16 01:08:03 +0530

    pool changes

--- job-admin-server/server.js
+++ job-admin-server/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
+const pool = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
